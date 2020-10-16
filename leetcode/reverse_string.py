3998b5e5768ef82851abaea2e91a39557f980716
Ahmed Hafez <gm.ahmed.hafez@gmail.com>
2020-10-16 04:10:43 +0200

    added swift code solution

--- leetcode/reverse_string.py
+++ leetcode/reverse_string.py
@@ -6,4 +6,22 @@ for i in range(length // 2):
     temp = input[i]
     input[i] = input[last_element_index - i] 
     input[last_element_index - i] = temp  
-print(input)    
+print(input) 
+
+
+# swift code solution 
+class Solution {
+    func reverseString(_ s: inout [Character]) {
+        let length = s.count
+        var lastElementIndex = length - 1 
+        let halfOfTheArray: Int = length / 2
+        for index in 0 ..< halfOfTheArray {
+            var temp = s[index] 
+            s[index] = s[lastElementIndex - index] 
+            s[lastElementIndex - index] = temp
+        
+    }
+        
+         
+    }
+}
