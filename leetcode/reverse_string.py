ac4e3a27797e8fcdf48fa67e03eab0c6afe087ae
Ahmed Hafez <gm.ahmed.hafez@gmail.com>
2020-10-23 11:12:39 +0200

    added solution using recursion

--- leetcode/reverse_string.py
+++ leetcode/reverse_string.py
@@ -10,18 +10,50 @@ print(input)
 
 
 # swift code solution 
-class Solution {
-    func reverseString(_ s: inout [Character]) {
-        let length = s.count
-        var lastElementIndex = length - 1 
-        let halfOfTheArray: Int = length / 2
-        for index in 0 ..< halfOfTheArray {
-            var temp = s[index] 
-            s[index] = s[lastElementIndex - index] 
-            s[lastElementIndex - index] = temp
+# class Solution {
+#     func reverseString(_ s: inout [Character]) {
+#         let length = s.count
+#         var lastElementIndex = length - 1 
+#         let halfOfTheArray: Int = length / 2
+#         for index in 0 ..< halfOfTheArray {
+#             var temp = s[index] 
+#             s[index] = s[lastElementIndex - index] 
+#             s[lastElementIndex - index] = temp
         
-    }
+#     }
         
          
-    }
-}
+#     }
+# }
+
+
+# printing the string in the reversed order
+def helper_reverse(index, word):
+    if word == None or index >= len(word):
+        return 
+    print(word[index])    
+    helper_reverse(index + 1, word) 
+    print(word[index]) 
+    
+
+
+def Reverse_String(word):
+    return helper_reverse(0, word) 
+
+
+
+# solving the problem using recursion 
+def reverseString(word):
+    def helper(left, right):
+        if left < right:
+            helper(left + 1, right - 1)
+            word[left], word[right] = word[right], word[left] 
+            
+
+    helper(0, len(word) - 1)        
+
+name = ["a", "h", "m", "e", "d"]
+print(name)
+x = reverseString(name)  
+print(name)
+
